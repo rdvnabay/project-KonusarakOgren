4754498a72deb6b3a6f4b2c404034eea1dc30127
Rıdvan <ridvanabay@gmail.com>
2021-06-23 18:22:56 +0300

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //
+        }
+    }
+}
---
+++ WebUI/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Net;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
